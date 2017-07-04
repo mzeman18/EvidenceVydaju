d948952f9e4169847c605b80cb3e222e8c49a409
Lukáš <Luk@Lukas>
2017-07-04 09:52:08 +0200

    fix conflict

--- src/main/java/cz/jcu/uai/praktikum/EvidenceVydaju/App.java
+++ src/main/java/cz/jcu/uai/praktikum/EvidenceVydaju/App.java
@@ -8,10 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-<<<<<<< HEAD
-        System.out.println( "Jednnanaa" );
-=======
+
+
         System.out.println( "kskgjsksjdgnkdskg!" );
->>>>>>> branch 'master' of https://github.com/mzeman18/EvidenceVydaju.git
+
     }
 }
