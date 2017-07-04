38fc5eb845731bc0bd6c5f0d77c32d49cd469ad9
Lukáš <Luk@Lukas>
2017-07-04 11:53:20 +0200

    Přidání komponent hlavního menu

--- src/main/java/cz/jcu/uai/praktikum/EvidenceVydaju/HlavniMenu.java
+++ src/main/java/cz/jcu/uai/praktikum/EvidenceVydaju/HlavniMenu.java
@@ -6,8 +6,10 @@ import java.awt.*;
 public class HlavniMenu {
 	JFrame hlavniMenu;
 	JPanel panel;
+	JLabel textOkno, obrazekOkno;
 	FlowLayout fl = new FlowLayout();
 	GridBagLayout gbl = new GridBagLayout();
+	GridBagConstraints gbc = new GridBagConstraints();
 	
 	public HlavniMenu() {
 		vytvorOkno();
@@ -25,6 +27,19 @@ public class HlavniMenu {
 		panel = new JPanel();
 		panel.setLayout(gbl);
 		panel.setBackground(Color.WHITE);
+		
+		textOkno = new JLabel("Vítejte v aplikaci Evidence Výdajů");
+		textOkno.setFont (new Font("Sefir", Font.BOLD, 18));
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.gridwidth = 3;
+		gbc.ipady = 15;
+		gbc.anchor = (GridBagConstraints.CENTER);
+		gbl.setConstraints(textOkno, gbc);
+		panel.add(textOkno);
+		
+		hlavniMenu.add(panel);
+		
 	}
 	
 	public static void main (String[] args) {
