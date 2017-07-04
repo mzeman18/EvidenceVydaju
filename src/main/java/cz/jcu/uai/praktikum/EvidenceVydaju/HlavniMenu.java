14c738657131c5ef9c7365dbec76f7250fd7f3cf
zemanm18 <zemi4444@gmail.com>
2017-07-04 11:34:20 +0200

    základ

--- src/main/java/cz/jcu/uai/praktikum/EvidenceVydaju/HlavniMenu.java
+++ src/main/java/cz/jcu/uai/praktikum/EvidenceVydaju/HlavniMenu.java
@@ -1,5 +1,37 @@
 package cz.jcu.uai.praktikum.EvidenceVydaju;
 
-public class HlavniMenu {
+import javax.swing.*;
+import java.awt.*;
 
+public class HlavniMenu {
+	JFrame hlavniMenu;
+	JPanel panel;
+	FlowLayout fl = new FlowLayout();
+	GridBagLayout gbl = new GridBagLayout();
+	
+	public HlavniMenu() {
+		vytvorOkno();
+	}
+	
+	private void vytvorOkno() {
+		hlavniMenu = new JFrame ("Evidence výdajů");
+		hlavniMenu.setSize(600, 600);
+		hlavniMenu.setVisible(true);
+		hlavniMenu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		hlavniMenu.setLocationRelativeTo(null);
+		hlavniMenu.setResizable(false);
+		hlavniMenu.setLayout(fl);
+		
+		panel = new JPanel();
+		panel.setLayout(gbl);
+		panel.setBackground(Color.WHITE);
+	}
+	
+	public static void main (String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				HlavniMenu menu = new HlavniMenu();
+			}
+		});
+	}
 }
